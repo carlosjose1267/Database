a019ea94115c7136c32d841c73daef573160e965
carlosjose1267 <36506011+carlosjose1267@users.noreply.github.com>
2020-06-14 22:46:25 +0200

    version2

--- Cursores.sql
+++ Cursores.sql
@@ -3,10 +3,11 @@
 ---------------------
 
 DECLARE
+-- Declaramos las variables 
    Cantidad_del_salario VARCHAR2(20);
    Nombre_del_oficio VARCHAR(20);
    Nombre_del_empleado VARCHAR(20);
-
+-- Creamos el cursos y metemos lo que nos devuelve la consulta (conjunto de filas) en el cursor "MenorSalario"
    CURSOR MenorSalario is   
     SELECT * FROM (
       SELECT employees.salary, jobs.job_title, first_name FROM employees
@@ -14,12 +15,10 @@ DECLARE
       where employees.job_id = 'ST_CLERK' ORDER BY employees.salary ASC
     ) WHERE rownum <= 2;
     
-
+-- Abrimos el cursor y creamos un bucle para extraer lo que nos devolvia la consulta y las metemos en las variables previamente creadas
+-- posteriormente lo sacamos pon la salida "output" (por pantalla) y cerramos el bucle for y el cursor.
 BEGIN
    OPEN MenorSalario;
-    Dbms_output.PUT_LINE(
-         'Empleados con menor salario del oficio Stock Clerk:'
-    );
    FOR i IN 1..2 LOOP
       FETCH MenorSalario INTO Cantidad_del_salario, Nombre_del_oficio, Nombre_del_empleado;
         Dbms_output.PUT_LINE(
